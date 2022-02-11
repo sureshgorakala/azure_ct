e45713615cf7bede17af28c9bfeddfe65814f5ef
harsha27191@gmail.com <harsha27191@gmail.com>
2022-02-11 11:52:30 +0000

    added compute target creation and deletion

---
+++ compute/.ipynb_checkpoints/create_compute_target-Copy1-checkpoint.py
@@ -0,0 +1,27 @@
+import os
+from azureml.core.compute import ComputeTarget, AmlCompute
+from azureml.core.compute_target import ComputeTargetException
+from azureml.core.authentication import ServicePrincipalAuthentication
+
+sp = ServicePrincipalAuthentication(tenant_id=os.environ['AML_TENANT_ID'],
+                                    service_principal_id=os.environ['AML_PRINCIPAL_ID'],
+                                    service_principal_password=os.environ['AML_PRINCIPAL_PASS'])
+ws = Workspace.get(name=os.environ["WORKSPACE_NAME"],
+               subscription_id=os.environ["SUBSCRIPTION_ID"],
+               resource_group=os.environ["RESOURCE_GROUP"],
+               auth=sp)
+
+cluster_name = "aml-cluster"
+
+try:
+    # Check for existing compute target
+    training_cluster = ComputeTarget(workspace=ws, name=cluster_name)
+    print('Found existing cluster, use it.')
+except ComputeTargetException:
+    # If it doesn't already exist, create it
+    try:
+        compute_config = AmlCompute.provisioning_configuration(vm_size='Standard_DS1_v2', max_nodes=2)
+        training_cluster = ComputeTarget.create(ws, cluster_name, compute_config)
+        training_cluster.wait_for_completion(show_output=True)
+    except Exception as ex:
+        print(ex)
